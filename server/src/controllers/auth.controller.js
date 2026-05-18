1844695969b3b79bec14655e3841477661778ae1
Ankit-976 <ankitbasa113@gmail.com>
2026-05-18 16:50:36 +0530

    created login api

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -9,6 +9,16 @@ const registerUser = async (req, res) => {
     return res.status(400).json({ message: "All fields are required" });
   }
 
+  const existingUser = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (existingUser) {
+    return res.status(400).json({ message: "User already exists" });
+  }
+
   const hashedPassword = await bcrypt.hash(password, 10);
 
   const user = await prisma.user.create({
@@ -38,6 +48,50 @@ const registerUser = async (req, res) => {
   })
 };
 
+const loginUser = async (req, res) => {
+
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (!user) {
+    return res.status(400).json({ message: "Invalid credentials" });
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    return res.status(400).json({ message: "Invalid credentials" });
+  }
+
+  const token = jwt.sign(
+    {
+      id: user.id,
+    },
+    process.env.JWT_SECRET_KEY,
+    {
+      expiresIn: "7d",
+    }
+  );
+
+  res.cookie("token", token);
+
+  res.status(200).json({
+    message: "User logged in successfully",
+    username: user.username,
+    email: user.email
+  });
+};
+
 module.exports = {
   registerUser,
+  loginUser,
 };
