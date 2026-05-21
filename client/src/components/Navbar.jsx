3cffe4d1c2172590e02aa993b7bb0cf14842e5f2
Ankit-976 <ankitbasa113@gmail.com>
2026-05-21 15:41:58 +0530

    Completed navbar and created user dropdown

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,31 +1,63 @@
+import { useState } from "react";
 import logo from "../assets/logo.png";
+import UserDropdown from "./UserDropdown";
 const Navbar = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isUserDropActive, setisUserDropActive] = useState(false);
+
+  const loginBtn = () => {
+    setIsLoggedIn(true);
+  };
+
+  const userDropdown = () => {
+    setisUserDropActive(!isUserDropActive)
+  }
+
   return (
-    <div className="py-3 flex justify-between items-center px-25">
-      <a href="/" className="flex items-center justify-center gap-2 cursor-pointer group">
+    <div className="py-3 flex justify-between items-center px-25 absolute top-0 left-0 w-full">
+      <a
+        href="/"
+        className="flex items-center justify-center gap-2 cursor-pointer group"
+      >
         <img
           src={logo}
           alt="Logo"
-          className=" rounded-xl transition-shadow duration-300 group-hover:shadow-[0_0_18px_rgba(239,68,68,0.8)]"/>
-        <span className="block text-xl">CineSphere</span>
+          className=" rounded-xl transition-shadow duration-300 group-hover:shadow-[0_0_18px_rgba(239,68,68,0.8)]"
+        />
+        <span className="block text-xl font-['Stack_Sans_Notch']">
+          CineSphere
+        </span>
       </a>
       <ul className="flex gap-15 text-[0.9rem] text-gray-500 font-semibold">
         <li className="text-gray-300 cursor-pointer">Home</li>
         <li className="hover:text-gray-300 cursor-pointer">Movies</li>
-        <li className="hover:text-gray-300 cursor-pointer">My Bookings</li>
+        {isLoggedIn && (
+          <li className="hover:text-gray-300 cursor-pointer">My Bookings</li>
+        )}
       </ul>
       <div className="flex items-center gap-3 font-semibold">
-        <button className="bg-transparent border-gray-800 border-[0.1px] rounded-full py-2 px-6 flex items-center justify-center text-[0.9rem] cursor-pointer">
-          Login
-        </button>
-        <button className="bg-red-600 rounded-full py-2 px-6 flex items-center justify-center text-[0.9rem] cursor-pointer">
-          Sign Up
-        </button>
-        <button className="rounded-full py-2 px-3 flex bg-transparent border-[0.1px] border-gray-800 hidden gap-2 items-center justify-center cursor-pointer">
-          <span className="block rounded-full bg-emerald-800 h-6 w-6">U</span>
-          <span>User</span>
-        </button>
+        {!isLoggedIn ? (
+          <>
+            <button
+              className="bg-transparent border-gray-800 border-[0.1px] rounded-full py-2 px-6 flex items-center justify-center text-[0.9rem] cursor-pointer"
+              onClick={loginBtn}
+            >
+              Login
+            </button>
+            <button className="bg-red-600 rounded-full py-2 px-6 flex items-center justify-center text-[0.9rem] cursor-pointer">
+              Sign Up
+            </button>
+          </>
+        ) : (
+          <button className="rounded-full py-2 px-3 flex bg-transparent border-[0.1px] border-gray-800 gap-2 items-center justify-center cursor-pointer" onClick={userDropdown}>
+            <span className="block rounded-full bg-emerald-800 h-6 w-6">U</span>
+            <span>User</span>
+          </button>
+        )}
       </div>
+      {isLoggedIn && isUserDropActive && (
+        <UserDropdown setIsLoggedIn={setIsLoggedIn} setisUserDropActive={setisUserDropActive}/>
+      )}
     </div>
   );
 };
---
+++ client/src/components/UserDropdown.jsx
@@ -0,0 +1,23 @@
+const UserDropdown = ({ setIsLoggedIn, setisUserDropActive }) => {
+  const logoutBtn = () => {
+    setIsLoggedIn(false);
+    setisUserDropActive(false);
+  };
+  return (
+    <div className="absolute bg-black top-15 right-25 flex flex-col gap-1 w-50 rounded-xl border-[0.1px] border-gray-600/50 font-['Nunito']">
+      <div className="flex flex-col px-5 pt-4">
+        <span className="text-[0.85rem]">Username</span>
+        <span className="text-[0.8rem] text-gray-500/70">Emailusernahah</span>
+      </div>
+      <hr className="w-[90%] m-auto text-gray-600/60" />
+      <div className="flex flex-col text-[0.9rem] px-5 pb-4 gap-2">
+        <span className="cursor-pointer">My Bookings</span>
+        <span className="text-red-700 cursor-pointer" onClick={logoutBtn}>
+          Logout
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default UserDropdown;
