5a097111c0e7b358eb084bb9a448740b33f6cac6
Ankit-976 <ankitbasa113@gmail.com>
2026-05-24 01:37:57 +0530

    Finished home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,52 +1,200 @@
-import bg from "../assets/interstellerlandscape.jpg";
-import { RiArrowRightCircleFill, RiArrowRightSLine, RiArrowLeftSLine } from "@remixicon/react";
-
+import {
+  RiArrowRightCircleFill,
+  RiArrowRightSLine,
+  RiArrowLeftSLine,
+} from "@remixicon/react";
+import api from "../utils/Api";
+import { useEffect, useState, useRef } from "react";
+import { gsap } from "gsap";
 
 const Home = () => {
+  const [movies, setMovies] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const titleRef = useRef(null);
+  const descRef = useRef(null);
+  const ratingTimeRef = useRef(null);
+  const bgRef = useRef(null);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const response = await api.get("/movie/getMovies");
+
+      setMovies(response.data.movies.slice(0, 4));
+    };
+
+    fetchMovies();
+  }, []);
+
+  const changeMovie = (newIndex) => {
+    gsap.to(
+      [titleRef.current, descRef.current, ratingTimeRef.current],
+
+      {
+        y: -10,
+        opacity: 0,
+
+        duration: 0.6,
+        ease: "power2.in",
+
+        stagger: 0.08,
+
+        onComplete: () => {
+          gsap.to(bgRef.current, {
+            opacity: 0,
+
+            duration: 0.5,
+            ease: "power2.in",
+          });
+          setCurrentIndex(newIndex);
+          gsap.fromTo(
+            bgRef.current,
+
+            {
+              opacity: 0,
+            },
+
+            {
+              opacity: 1,
+
+              duration: 1,
+              ease: "power2.out",
+            },
+          );
+
+          gsap.fromTo(
+            [titleRef.current, descRef.current, ratingTimeRef.current],
+
+            {
+              y: 10,
+              opacity: 0,
+            },
+
+            {
+              y: 0,
+              opacity: 1,
+
+              duration: 0.8,
+              ease: "power3.out",
+
+              stagger: 0.08,
+            },
+          );
+        },
+      },
+    );
+  };
+
+  useEffect(() => {
+    gsap.set(".movieline",{ scaleX: 0, transformOrigin: "left" })
+    gsap.to(`#movieline${currentIndex}`, {
+      scaleX: 1,
+      duration: 6,
+      delay:1,
+      transformOrigin: "left",
+      ease:"none"
+    })
+  
+  }, [movies, currentIndex])
+  
+
+  useEffect(() => {
+    if (!movies.length) return;
+
+    const interval = setInterval(() => {
+      const nextIndex =
+        currentIndex === movies.length - 1 ? 0 : currentIndex + 1;
+
+      changeMovie(nextIndex);
+    }, 6000);
+
+    return () => clearInterval(interval);
+  }, [movies, currentIndex]);
+
+  const currentMovie = movies[currentIndex];
+
   return (
     <div>
       <div
+        ref={bgRef}
         className="h-[90%] overflow-hidden w-full bg-cover bg-top absolute top-0 left-0"
-        style={{ backgroundImage: `url(${bg})` }}
+        style={{ backgroundImage: `url(${currentMovie?.posterUrl})` }}
       >
-        <div className="text-white h-full flex flex-col p-25 justify-end gap-6">
+        <div className="text-white h-full flex flex-col px-25 py-20 justify-end gap-6">
           <div className="flex gap-5 font-['Nunito'] text-[0.85rem] tracking-wider">
-            <span className="font-bold text-red-500 "><span className="font-bold">•</span> NOW SHOWING</span>
-            <span className="font-bold text-[#363333]">SCIENCE FICTION</span>
-          </div>
-          <div>
-            <span className="text-9xl tracking-wide font-bold font-[Bebas_Neue]">INTERSTELLAR</span>
-          </div>
-          <div className="w-[40%] font-[Nunito]">
-            <span className="font-semibold text-[#8a8a8a]">
-              Interstellar is a 2014 sci-fi film where a pilot enters a black
-              hole to communicate with his daughter across time and dimensions,
-              ultimately saving humanity.
+            <span className="font-bold text-red-500 ">
+              <span className="font-bold">•</span> NOW SHOWING
+            </span>
+            <span className="font-bold text-[#8a8a8a]">
+              {currentMovie?.genre.toUpperCase()}
             </span>
           </div>
-          <div className="flex gap-5 text-[#8a8a8a] font-[Nunito] text-[0.9rem] font-bold">
+          <div>
+            <span
+              ref={titleRef}
+              className="block text-9xl tracking-wide font-bold font-[Bebas_Neue]"
+            >
+              {currentMovie?.title}
+            </span>
+          </div>
+          <div className="w-[40%] font-[Nunito]">
+            <span ref={descRef} className=" block font-semibold text-[#8a8a8a]">
+              {currentMovie?.description}
+            </span>
+          </div>
+          <div
+            ref={ratingTimeRef}
+            className="flex gap-5 text-[#8a8a8a] font-[Nunito] text-[0.9rem] font-bold"
+          >
             <span>⭐️ 8.2/10</span>
-            <span>2h22m</span>
-            <span>English</span>
+            <span>{currentMovie?.duration}</span>
+            <span>{currentMovie?.language}</span>
           </div>
           <div className="flex gap-3 font-semibold">
-              <button className="bg-red-600 rounded-full py-2 px-5 flex items-center justify-center text-[0.9rem] cursor-pointer gap-2">
-                <RiArrowRightCircleFill size='20' />Book Tickets
-              </button>
-              <button className="border-gray-800 border-[0.1px] rounded-full py-2 px-7 flex items-center justify-center text-[0.9rem] cursor-pointer bg-black/10">
-                More Details
-              </button>
+            <button className="bg-red-600 rounded-full py-2 px-5 flex items-center justify-center text-[0.9rem] cursor-pointer gap-2">
+              <RiArrowRightCircleFill size="20" />
+              Book Tickets
+            </button>
+            <button className="border-gray-800 border-[0.1px] rounded-full py-2 px-7 flex items-center justify-center text-[0.9rem] cursor-pointer bg-black/10">
+              More Details
+            </button>
           </div>
           <div className="flex justify-between items-center">
             <div className="flex gap-2">
-              <span className="block bg-[#323232] h-1 rounded-3xl w-10"></span>
-              <span className="block bg-[#323232] h-1 rounded-3xl w-10"></span>
-              <span className="block bg-[#323232] h-1 rounded-3xl w-10"></span>
-              <span className="block bg-[#323232] h-1 rounded-3xl w-10"></span>
+              {movies.map((_, idx) => {
+                return (
+                  <div 
+                    key={idx}
+                  className="relative">
+                  <span
+                    className={`block h-1 rounded-3xl w-15 bg-[#323232]`}></span>
+                    <span 
+                    id={`movieline${idx}`} 
+                    className={`block movieline absolute top-0 left-0 h-1 rounded-3xl w-15 ${currentIndex == idx ? "bg-red-500" : "bg-[#323232]"} `}></span>
+                  </div>
+                );
+              })}
             </div>
             <div className="flex gap-1">
-              <span className="block bg-[#373434] h-fit w-fit p-2 rounded-full border border-[#505050]"><RiArrowLeftSLine /></span>
-              <span className="block bg-[#373434] h-fit w-fit p-2 rounded-full border border-[#505050]"><RiArrowRightSLine /></span>
+              <span
+                onClick={() =>
+                  changeMovie(
+                    currentIndex === 0 ? movies.length - 1 : currentIndex - 1,
+                  )
+                }
+                className="block bg-[#373434] h-fit w-fit p-2 rounded-full border border-[#505050] cursor-pointer"
+              >
+                <RiArrowLeftSLine />
+              </span>
+              <span
+                onClick={() =>
+                  changeMovie(
+                    currentIndex === movies.length - 1 ? 0 : currentIndex + 1,
+                  )
+                }
+                className="block bg-[#373434] h-fit w-fit p-2 rounded-full border border-[#505050] cursor-pointer"
+              >
+                <RiArrowRightSLine />
+              </span>
             </div>
           </div>
         </div>
